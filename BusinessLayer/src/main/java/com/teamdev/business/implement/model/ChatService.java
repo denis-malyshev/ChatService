715e8f4b94d25054c9813d82a031d06966ab45ec
Denis Malyshev <denis.malyshev@teamdev.com>
2015-12-22 19:15:56 +0200

    Save. Tried to set up maven build setting because spring ruined maven build.

--- BusinessLayer/src/main/java/com/teamdev/business/implement/model/ChatService.java
+++ BusinessLayer/src/main/java/com/teamdev/business/implement/model/ChatService.java
@@ -5,16 +5,10 @@ import com.teamdev.business.implement.AuthenticationServiceImpl;
 import com.teamdev.business.implement.ChatRoomServiceImpl;
 import com.teamdev.business.implement.MessageServiceImpl;
 import com.teamdev.business.implement.UserServiceImpl;
-import com.teamdev.persistence.dom.User;
 import com.teamdev.persistence.repository.RepositoryFactory;
-import org.apache.log4j.Logger;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class ChatService {
-
-    private static final Logger logger = Logger.getLogger(ChatService.class);
 
     private final RepositoryFactory repositoryFactory;
 
@@ -33,7 +27,7 @@ public class ChatService {
     }
 
     public static void main(String[] args) throws Exception {
-        ApplicationContext businessContext = new ClassPathXmlApplicationContext("META-INF/beansBusiness.xml");
+//        ApplicationContext businessContext = new ClassPathXmlApplicationContext("META-INF/beansBusiness.xml");
 //        ApplicationContext persistenceContext = new ClassPathXmlApplicationContext("META-INF/beansPersistence.xml");
 //        ApplicationContext repositoryContext = new ClassPathXmlApplicationContext("META-INF/beansRepository.xml");
     }
