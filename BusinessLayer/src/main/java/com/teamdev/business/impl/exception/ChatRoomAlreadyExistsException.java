73c25fc8d05ac3fdcc04d088cc5027478dc80413
Denis Malyshev <denis.malyshev@teamdev.com>
2016-01-03 23:43:15 +0200

    Added 3 exception classes.

---
+++ BusinessLayer/src/main/java/com/teamdev/business/impl/exception/ChatRoomAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.teamdev.business.impl.exception;
+
+public class ChatRoomAlreadyExistsException extends Exception {
+
+    public ChatRoomAlreadyExistsException(String message) {
+        super(message);
+    }
+}
---
+++ BusinessLayer/src/main/java/com/teamdev/business/impl/exception/ChatRoomNotFoundException.java
@@ -0,0 +1,8 @@
+package com.teamdev.business.impl.exception;
+
+public class ChatRoomNotFoundException extends Exception {
+
+    public ChatRoomNotFoundException(String message) {
+        super(message);
+    }
+}
